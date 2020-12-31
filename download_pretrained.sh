e7f5a39aa7ad01004234831ef84be9256763882e
Anh DH <anhdhbn@gmail.com>
2020-12-31 10:26:09 +0700

    rm sth

--- download_pretrained.sh
+++
@@ -1,6 +0,0 @@
-rm -rf pretrained_models
-mkdir pretrained_models
-
-python download_pretrained.py pretrained_models/resnet50.model 1-tEtYQdT1G9kn8HsqKNDHVqjE16F8YQH
-python download_pretrained.py pretrained_models/mobilev2.model 1lPiRjFvajwrhOHVuXrygAj2cRb2BFFfz
-python download_pretrained.py pretrained_models/alex.model 1e51IL1UZ-5seum2yUYpf98l2lJGUTnhs
